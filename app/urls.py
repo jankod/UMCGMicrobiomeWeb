bdcff09fb2245c9a42540c7813cc6165a83c369d
Janko <jdiminic@gmail.com>
2018-11-23 22:01:40 +0100

    init

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.demo_coreui1, name='demo1'),
-
+    path('page1', views.page1, name='page1'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def demo_coreui1(request):
     return render(request, 'demo_coreui1.html')
+
+
+def page1(request):
+    return render(request, 'page1.html')
