b77da1bc4b94ad2af4b2d07addd65591d33021fc
Janko <jdiminic@gmail.com>
2018-12-06 23:46:01 +0100

    dddd

--- UMCGMicrobiomeWeb/wsgi.py
+++ UMCGMicrobiomeWeb/wsgi.py
@@ -12,10 +12,14 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
+
+from sys import platform
+if platform == "linux" or platform == "linux2":
+    sys.path.append('/home/tag/umcg_web/UMCGMicrobiomeWeb/UMCGMicrobiomeWeb')
+    sys.path.append('/home/tag/umcg_web/UMCGMicrobiomeWeb')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UMCGMicrobiomeWeb.settings')
 
-
-
-sys.path.append('/home/django_projects/MyProject')
+#sys.path.append('/home/django_projects/MyProject')
 
 application = get_wsgi_application()
